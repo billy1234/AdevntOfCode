6bd6817d9412aff4e8e9a8596d60cf97824f7ed1
Billy Mihalarias <bmihalarias@gmail.com>
2023-06-10 15:54:03 +1000

    added file input

--- GO/day1/main.go
+++ GO/day1/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err)
+	}
 }
  
