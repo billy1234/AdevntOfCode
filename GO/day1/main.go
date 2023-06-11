5c524bfe4654725bd61589d3795ba5e36fbfaec6
Billy Mihalarias <bmihalarias@gmail.com>
2023-06-11 16:41:18 +1000

    completed day 1

--- GO/day1/main.go
+++ GO/day1/main.go
@@ -4,18 +4,42 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-
 	scanner := bufio.NewScanner(os.Stdin)
+	
+	var elfNum, winningElf int = 0, 0;
+
+	var maxCalories, currentCalories int = 0, 0;
 
 	for scanner.Scan(){
-		fmt.Println(scanner.Text())
+		currLine := scanner.Text()
+
+		
+		if currLine == ""{
+			elfNum++
+			currentCalories = 0
+		} else {
+			i, err := strconv.Atoi(currLine)
+			if err != nil {
+				fmt.Println("bad line, cant parse int.")
+				panic(err)
+			}
+			currentCalories += i;
+
+			if currentCalories >= maxCalories{
+				maxCalories = currentCalories
+				winningElf = elfNum
+			}
+		}
+			
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error: ", err)
 	}
+
+	fmt.Println("Winner: ",winningElf, " Calories: ", maxCalories);
 }
  
