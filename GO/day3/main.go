fc63ad2923eeeca4ad6f4a1ccf15ad868f6d9c64
Billy Mihalarias <bmihalarias@gmail.com>
2023-07-15 14:18:32 +1000

    GO: Added day 3 part 2

--- GO/day3/main.go
+++ GO/day3/main.go
@@ -26,16 +26,33 @@ func getPriority(char byte) int {
 	}
 }
 
+func findShared(a string, b string, c string) byte {
+	for _, a_char := range a {
+		for _, b_char := range b {
+			for _, c_char := range c {
+				if (a_char == b_char && b_char == c_char) {
+					return byte(a_char) //Wont work for characters > 1 byte
+				}
+			}
+		}	
+	}
+	return 0x00
+}
+
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	total := 0
 
+	var lines []string	
+
 	for scanner.Scan() {
 		currLine := scanner.Text()
 
 		if currLine != "" {
+			lines = append(lines, currLine)
+
 			char := getSharedChar( currLine[0: len(currLine)/2], currLine[len(currLine)/2:len(currLine)])
 			total += getPriority(char)
 		}
@@ -45,6 +62,23 @@ func main() {
 		fmt.Println(os.Stderr, "Error: ", err)
 		panic(err)
 	}
+
 	fmt.Println("Part 1:", total)
+
+	badgeTotal := 0
+	
+	if len(lines) % 3 == 0 {
+		for  i := 0; i < len(lines); i+= 3 {
+			badgeTotal += getPriority(findShared(
+				lines[i],
+				lines[i + 1],
+				lines[i + 2]))
+		}
+	} else{
+		fmt.Println(os.Stderr,"Input not divisible by 3, input len: ", len(lines))
+	}
+
+	fmt.Println("Part 2:",badgeTotal)
+
 }
 
