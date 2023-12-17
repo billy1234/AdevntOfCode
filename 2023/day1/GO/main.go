04f750a239d8df6488d5a3bd25672ae6d09f18a1
unknown <bmihalarias@gmail.com>
2023-12-17 16:36:02 +1100

    finised day 1 part 2

--- 2023/day1/GO/main.go
+++ 2023/day1/GO/main.go
@@ -4,46 +4,90 @@ import (
 	"fmt"
 	"bufio"
 	"os"
-	"strconv"
 )
 
+var numberStrings = []string{
+	"one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+}
 
+
+func isAtN(x string, pos int) (result int, skip int) {
+	for strNum := 0; strNum < len(numberStrings); strNum++ {
+		for i := pos; i < len(x); i++ {
+			if (numberStrings[strNum][i - pos] != x[i]) {
+				break;
+			}
+		
+			if (i - pos == len(numberStrings[strNum]) - 1) {
+				return strNum + 1, (i - pos - 1);
+			}
+		}
+	}
+	return -1, 0
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var total int = 0
+	var total1, total2 int = 0, 0
 	for scanner.Scan() {
 		currLine := scanner.Text()
 
 		if currLine != "" {
-			var a , b rune = ' ', ' ';
-			for _, e := range currLine {
+			var a , b int = -1, -1;
+			var c, d int = -1, -1;
+			for i := 0; i < len(currLine); i ++{
+				e := currLine[i]
 				if ( byte(e) >= 48 && byte(e) <= 57){
-					if (a == ' '){
-						a = e;
+					if (a == -1){
+						a = int(e - '0');
 					}else{
-						b = e;
+						b = int(e - '0');
 					}
+
+					if (c == -1){
+						c = int(e - '0')
+					}else{
+						d = int(e - '0')
+					}
+				} else if (byte(e) >= 97 && byte(e) <= 122){
+					res, skip := isAtN(currLine,i)
+					if (res != -1){
+						if (c == -1){
+							c = res
+						} else {
+							d = res
+						}
+					i += skip
+					}
+					//try to match the strings
 				}
 			}
-			if b == ' ' {
+			if b == -1 {
 				b = a
 			}
-			var res string = string([]rune{a,b})
-			i, err := strconv.Atoi(res)
-			if (err != nil){
-				panic(err)
-			}			
-			total += i
+			if d == -1 {
+				d = c
+			}
+			total1 += (a * 10) + b
+			total2 += (c * 10) + d
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println(os.Stderr, "Error: ", err)
+		fmt.Println("Error: ",err)
 		panic(err)
 	}
-	fmt.Println(total)
+	fmt.Println("Part 1: ", total1)
+	fmt.Println("Part 2: ", total2)
 
 }
 
